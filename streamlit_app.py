90e765ff2d4bc7c6b75ff72f16c23ce52991febc
kienlv88 <kienlv88@gmail.com>
2025-08-12 16:51:56 +0700

    m
    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ for message in st.session_state.messages:
         st.markdown(f'<div class="user">{message["content"]}</div>', unsafe_allow_html=True)
 
 # Ô nhập liệu cho người dùng
-if prompt := st.chat_input("Bạn nhập nội dung vào đây nhé?"):
+if prompt := st.chat_input("Bạn nhập nội dung quan tâm vào đây nhé?"):
     # Lưu tin nhắn người dùng vào session
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.markdown(f'<div class="user">{prompt}</div>', unsafe_allow_html=True)
